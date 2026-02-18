f91b78fb23b57af925e90df241f60222d7f9fdc1
kellec8 <kellec8@mail.uc.edu>
2026-02-18 13:32:33 -0500

    create react app

---
+++ react/cookingware/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="flex:1 max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+        Cooking Ware
+      </h1>
+    </div>
+  );
+}
